a3dfc8bd43281ebaa8b843ddb2ddc152bf7df04d
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-12-12 16:07:04 -0800

    tests passing, need to fix frontend

--- counter-contract/tests/harness.rs
+++ counter-contract/tests/harness.rs
@@ -35,8 +35,16 @@ async fn get_contract_instance() -> (MyContract, ContractId) {
 
 #[tokio::test]
 async fn can_get_contract_id() {
-    let (_instance, _id) = get_contract_instance().await;
-    
+    let (instance, _id) = get_contract_instance().await;
+     // Increment the counter
+     let _result = instance.methods().increment().call().await.unwrap();
+
+     // Get the current value of the counter
+     let result = instance.methods().count().call().await.unwrap();
+ 
+     // Check that the current value of the counter is 1.
+     // Recall that the initial value of the counter was 0.
+     assert_eq!(result.value, 1);
 
     // Now you have an instance of your contract you can use to test each function
 }
