51e5c40c9a80fdb8753752b3d08b329abcaaecc4
cold-briu <andresf.xyz@gmail.com>
2023-10-03 20:59:07 -0500

    feat: use fuel wallet hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,109 +1,74 @@
+import { useIsConnected, useAccount, useConnect, useWallet } from "@fuel-wallet/react";
 import { useEffect, useState } from "react";
 import "./App.css";
 // Import the contract factory -- you can find the name in index.ts.
 // You can also do command + space and the compiler will suggest the correct name.
-import { CounterContractAbi__factory } from "./contracts";
- 
+import { CounterContractAbi, CounterContractAbi__factory } from "./contracts";
+
 // The address of the contract deployed the Fuel testnet
 const CONTRACT_ID =
   "0xa0f2b97f32300d7360b5632e682c1508b326da9a00fbe755646d3809aec90672";
- 
+
 function App() {
-  const [connected, setConnected] = useState<boolean>(false);
-  const [account, setAccount] = useState<string>("");
+  const { isConnected } = useIsConnected();
+  const { connect } = useConnect();
+  const { account } = useAccount();
+  const { wallet } = useWallet({ address: account });
   const [counter, setCounter] = useState<number>(0);
-  const [loaded, setLoaded] = useState(false);
- 
+  const [contract, setContract] = useState<CounterContractAbi | null>(null);
+
   useEffect(() => {
-    setTimeout(() => {
-      checkConnection();
-      setLoaded(true);
-    }, 200)
-    if (connected) getCount();
-  }, [connected, getCount])
- 
-  async function connect() {
-    if (window.fuel) {
-      try {
-        await window.fuel.connect();
-        const [account] = await window.fuel.accounts();
-        setAccount(account);
-        setConnected(true);
-      } catch (err) {
-        console.log("error connecting: ", err);
-      }
+    if (isConnected) getCount()
+  }, [isConnected, getCount])
+
+  useEffect(() => {
+    if (wallet) {
+      const contractInstance = CounterContractAbi__factory.connect(CONTRACT_ID, wallet);
+      setContract(contractInstance);
+    }
+  }, [wallet, setContract]);
+
+  async function handleClick() {
+    if (isConnected) {
+      await increment();
+    } else {
+      await connect("");
     }
   }
- 
-  async function checkConnection() {
-    if (window.fuel) {
-      const isConnected = await window.fuel.isConnected();
-      if (isConnected) {
-        const [account] = await window.fuel.accounts();
-        setAccount(account);
-        setConnected(true);
-      }
-    }
-  }
- 
+
   async function getCount() {
-    if (window.fuel) {
-      const wallet = await window.fuel.getWallet(account);
-      const contract = CounterContractAbi__factory.connect(CONTRACT_ID, wallet);
-      const { value } = await contract.functions.count().simulate();
-      setCounter(value.toNumber());
+    if (wallet && contract) {
+      try {
+        const { value } = await contract.functions.count().simulate();
+        setCounter(value.toNumber());
+      } catch (error) {
+        console.log("error getting state:", error);
+
+      }
     }
   }
- 
+
   async function increment() {
-    if (window.fuel) {
-      const wallet = await window.fuel.getWallet(account);
-      const contract = CounterContractAbi__factory.connect(CONTRACT_ID, wallet);
-      // Creates a transactions to call the increment function
-      // because it creates a TX and updates the contract state this requires the wallet to have enough coins to cover the costs and also to sign the Transaction
+    if (wallet && contract) {
       try {
         await contract.functions.increment().txParams({ gasPrice: 1 }).call();
         getCount();
-      } catch (err) {
-        console.log("error sending transaction...", err);
+      } catch (error) {
+        console.log("error sending transaction: ", error);
       }
     }
   }
- 
-  if (!loaded) return null
- 
+
   return (
     <>
       <div className="App">
-        {
-          connected ? (
-            <>
-              <h3>Counter: {counter}</h3>
-              <button style={buttonStyle} onClick={increment}>
-                Increment
-              </button>
-            </>
-          ) : (
-            <button style={buttonStyle} onClick={connect}>Connect</button>
-          )
-        }
+        <h3>Counter: {counter}</h3>
+        <button onClick={handleClick}>
+          {isConnected ? "Increment" : "Connect"}
+        </button>
       </div>
     </>
   );
 }
- 
+
 export default App;
- 
-const buttonStyle = {
-  borderRadius: "48px",
-  marginTop: "10px",
-  backgroundColor: "#03ffc8",
-  fontSize: "20px",
-  fontWeight: "600",
-  color: "rgba(0, 0, 0, .88)",
-  border: "none",
-  outline: "none",
-  height: "60px",
-  width: "400px",
-  cursor: "pointer"
-}
